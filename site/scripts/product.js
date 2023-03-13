8b65b81f45e07217a557c85ca91f8a7abacd842b
Carter Awbrey <94002456+cawbrey@users.noreply.github.com>
2023-03-12 22:21:31 -0500

    modified formatting for product page

--- site/scripts/product.js
+++ site/scripts/product.js
@@ -16,24 +16,17 @@ function LoadPage(catalog){
         let imgLocation = "./assets/toilet_pictures/" + product["Picture"];
         
         elements_container.innerHTML +=
-            `<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-                <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" style="width: 100%">
-                    <div class="my-3 py-3">
-                        <h2 class="display-5">${product["Brand"]}</h2>
-                        <p class="lead">${product["Product Name"]}</p>
-                        <p class="lead"><b><em>${product["Price"]}</em></b></p>
-                    </div>
-                    <div class="bg-light box-shadow mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
-                        <img src="${imgLocation}" class="bg-light box-shadow mx-auto" style="width: 100%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-                    </div>
-                <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden" style="width: 100%">
-                    <div class="my-3 p-3">
-                        <p class="lead">${product["Description"]}</p>
-                        <p class="lead">Toilet Variant : ${product["Toilet Type"]}</p>
-                        <p class="lead">Average Rating of <i> ${product["Rating"]} / 5 </i></p>
-                        <!-- add star rating instead of text eventually -->
-                    </div>
-                 </div>
+            `<div class="col-lg-6">
+                <img src="${imgLocation}" class="rounded-4 img-fluid">
+            </div>
+            <div class="col-lg-6 text-center">
+                <h2 class="display-3">${product["Brand"]}</h2>
+                <p class="display-5">${product["Product Name"]}</p>
+                <p class="lead"><b><em>${product["Price"]}</em></b></p>
+                <p class="lead">${product["Description"]}</p>
+                <p class="lead">Toilet Variant : ${product["Toilet Type"]}</p>
+                <p class="lead">Average Rating of <i> ${product["Rating"]} / 5 </i></p>
+                <!-- add star rating instead of text eventually -->
             </div>
             `;
 }
